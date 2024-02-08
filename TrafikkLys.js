310407ce74091a0e7de9de5e90f4fbd620961ff7
William <157686898+xEdgeLordx@users.noreply.github.com>
2024-02-08 13:10:30 +0100

    Initial commit

---
+++ TrafikkLys.js
@@ -0,0 +1,38 @@
+allgray()
+        function updateView(onclick, color1, color2, color3){
+            app = document.getElementById('app');
+            app.innerHTML = /*html*/ `
+            <div class = "container"><div class = "trafikklys" onclick = "${onclick}()">
+                ${CreateLight(color1)}
+                ${CreateLight(color2)}
+                ${CreateLight(color3)}
+                </div></div>
+            `;
+        }
+        function CreateLight(color){
+            return /*html*/`<div class = "light" style = "background-color: ${color}"></div>`;
+        }
+        function allgray(){
+            updateView('RedLight','gray','gray','gray');
+
+        }
+        function stop(){
+            location.reload();
+        }
+        function RedLight(){
+            LightActive = true;
+            updateView('stop','red','gray','gray');
+            setTimeout(redyellow, 2000)
+        }
+        function redyellow(){
+            updateView('stop','red','yellow','gray');
+            setTimeout(greenlight, 2000)
+        }
+        function greenlight(){
+            updateView('stop', 'gray','gray','green')
+            setTimeout(yellowtored, 2000)
+        }
+        function yellowtored(){
+            updateView('RedLight', 'gray','yellow','gray')
+            setTimeout(RedLight, 2000)
+        }
